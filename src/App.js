1a975d2dd7127ac52cfd29f6c188c6ebc8ba1203
Joycenjichile <joycenkimalantoi@gmail.com>
2022-10-12 23:27:17 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Bloodrequest from './Bloodrequest';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Bloodrequest/>
+    </div>
+  );
+}
+
+export default App;
